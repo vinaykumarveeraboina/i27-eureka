feccb2fc5b61b4047d2312cc35700de33d2968b7
siva <siva@google.com>
2023-09-15 07:26:24 +0530

    removed code smells

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -1,18 +1,5 @@
 package com.lerner.eureka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
