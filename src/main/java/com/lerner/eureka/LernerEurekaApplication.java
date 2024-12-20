bbeb16e25070a824f81c1bae12bf09624ba3a9c4
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-12-20 17:05:35 +0530

    ded

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -4,20 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-//etra imports for test till java.util.logging.Logger;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.ResponseBody; 
-import java.util.ArrayList; 
-import java.util.List; 
-import java.util.HashMap; 
-import java.util.Map; 
-import java.util.logging.Logger;
-
 @SpringBootApplication
 @EnableEurekaServer
 public class LernerEurekaApplication {
